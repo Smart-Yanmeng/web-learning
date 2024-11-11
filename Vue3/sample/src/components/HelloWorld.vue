18f02f77d4ff513a1c820ebeeae7ee3f1626deba
Smart_Yanmeng <York.Yanmeng@gmail.com>
2024-11-11 19:03:09 +0800

    2024/11/11

--- Vue3/sample/src/components/HelloWorld.vue
+++ Vue3/sample/src/components/HelloWorld.vue
@@ -1,41 +1,33 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {computed, ref} from "vue";
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+let firstName = ref('york')
+let lastName = ref('smith')
+let fullName = computed({
+  get() {
+    return firstName.value.slice(0,1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+  },
+  set(val) {
+    const [str1, str2] = val.split('-')
+    firstName.value = str1
+    lastName.value = str2
+  }
+})
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <input placeholder="First Name" v-model="firstName"/>
+  <input placeholder="Last Name" v-model="lastName"/>
+  <h1>
+    <hr>
+    {{ fullName }}
+  </h1>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+input {
+  height: 30px;
+  font-size: medium;
+  margin: 0 10px;
 }
 </style>
